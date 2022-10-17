7c31ffa9509602533269b0ab1d8eea7804d31773
Csh4rpEnjoyer <113981795+Csh4rpEnjoyer@users.noreply.github.com>
2022-10-17 15:13:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// 2 variant
+Random r = new Random();
+Random s = new Random();
+int k = 0;
+int n = s.Next(10,20);
+int[] A = new int[n];
+for (int i = 0; i < n; i++)
+    A[i] = r.Next(20);
+for (int j = 1; j < n; j++)
+    if (A[j] > A[j - 1] && A[j] > A[j + 1])
+        k += 1;
+for (int i = 0; i < n; i++)
+    Console.WriteLine(A[i]);
+Console.WriteLine("Количество чисел, которые больше соседей:");
+Console.WriteLine(k);
